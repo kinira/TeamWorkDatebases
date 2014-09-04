549d7b823a0e446ddb9753c705af307dc3a70535
krisned <florentina2@abv.bg>
2014-09-04 12:10:38 +0300

    Add method for extracting zip files

---
+++ TeamWorkDatebases/LoadExcelReportsFromZipFile/ExtractZipFiles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace LoadExcelReportsFromZipFile
+{
+    public class ExtractZipFiles
+    {
+        public void ExtractFile()
+        {           
+            string zipPath = @"..\..\..\Sample-Sales-Reports.zip";
+            string extractPath = @"..\..\..\ExtractedZip\";
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
