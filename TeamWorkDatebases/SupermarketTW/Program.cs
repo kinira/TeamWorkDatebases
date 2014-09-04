41894d859fb5010c4e90ef095a8a8cf187bd2d7c
timenov <niko_dimitroff@yahoo.com>
2014-09-04 15:49:48 +0300

    Adding MongoDB models and console client for testing.

--- TeamWorkDatebases/SupermarketTW/Program.cs
+++ TeamWorkDatebases/SupermarketTW/Program.cs
@@ -1,15 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SupermarketTW
+﻿namespace SupermarketTW
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+
+    using SupermarketTW.Data;
+    using SupermarketTW.Models.SQL;
+    using SupermarketTW.Data.Migrations;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<SupermarketTWDbContext, Configuration>());
+            var db = new SupermarketTWDbContext();
+
+            var measure = db.Measures.First(m => m.Name.Equals("liters"));
+
+            var vendor = db.Vendors.First(v => v.Name.Equals("THE COCA COLA COMPANY"));
+
+            var product1 = new Product()
+            {
+                Name = "Coca cola Light",
+                BasePrice = 1.92,
+                Measure = measure,
+                Vendor = vendor
+            };
+
+            db.Products.Add(product1);
+            db.SaveChanges();
+
+            var products = db.Products.ToList();
+
+            foreach (var product in products)
+            {
+                System.Text.StringBuilder output = new System.Text.StringBuilder();
+                output.AppendLine("id: " + product.Id);
+                output.AppendLine("name: " + product.Name);
+                output.AppendLine("price: " + product.BasePrice);
+                output.AppendLine("vendor: " + product.Vendor);
+                output.AppendLine("measure: " + product.Measure);
+                output.AppendLine(new string('-', 50));
+
+                Console.WriteLine(output.ToString());
+            }
         }
     }
 }
